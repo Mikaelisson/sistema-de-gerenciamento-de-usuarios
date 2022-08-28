d89b2d6efe3ef3c8259e678540dcebe1557f9ad5
Mikaelisson <mikaelissongesuino@gmail.com>
2022-08-28 15:01:46 -0300

    add editar front-end

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
+const User = require("../models/User");
 
 router.get("/", controller.dataSearch);
+router.get("/edit/:id", async (req, res) => {
+  let id = req.params.id;
+  if (!id) id = req.body.id;
+  try {
+    const doc = await User.findById(id);
+    res.render("edit", { doc });
+  } catch (error) {
+    res.status(404).send("Error ao editar usuário");
+  }
+});
 
 router.post(
   "/register",
